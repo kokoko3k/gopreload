67ab53a489991fa7fb6ed7ca34f8fac43e52786b
Antonio Orefice <kokoko3k@gmail.com>
2016-03-17 15:50:50 +0100

    New *handy* script: Prepare_batch.sh It basically updates the file lists under enabled/ directory in a fully automatic way. It is useful after system upgrades.

---
+++ usr/share/gopreload/bin/Prepare_batch.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# * this file is part of GOPreload
+# * by Antonio Orefice
+# * Released under the GNU General Public License (GPL) version 2.
+
+IFS=$'\n'
+
+source /etc/gopreload.conf
+
+FILELISTS="$INSTALLDIR/enabled/*.$EUID.openfiles-*.txt"
+
+for PROG in $(head -q -n 1 $FILELISTS|cut -d "=" -f 2-) ; do
+    sh -c "$INSTALLDIR/bin/Prepare_unattended.sh $PROG"
+done
